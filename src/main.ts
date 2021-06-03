9b6bba766ecbc1ca83107ccaf754b070424e803c
KattemChen <1043641135@qq,com>
2021-06-03 10:06:02 +0800

    初始化仓库,加入哈士奇

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
